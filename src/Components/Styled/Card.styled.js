5d49c99bd6affecd7def289d158180ecb37d32d0
Albert <ishengomaalbert@gmail.com>
2021-12-23 06:54:21 +0300

    Fix accesibility issues

--- src/Components/Styled/Card.styled.js
+++ src/Components/Styled/Card.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledCard = styled.div`
+export const StyledCard = styled.section`
   display: flex;
   align-items: center;
   justify-content: space-between;
