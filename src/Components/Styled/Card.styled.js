25a752718122a506db5ce5d1e9c102fa1b22d8fc
Albert <ishengomaalbert@gmail.com>
2021-12-23 07:02:43 +0300

    Fix accesibility issues

--- src/Components/Styled/Card.styled.js
+++ src/Components/Styled/Card.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledCard = styled.div`
+export const StyledCard = styled.article`
   display: flex;
   align-items: center;
   justify-content: space-between;
--- src/Components/Styled/Container.styled.js
+++ src/Components/Styled/Container.styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.section `
+export const Container = styled.div `
   width:1200px;
   max-width:100%;
   padding:0 20px;
